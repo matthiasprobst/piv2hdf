62fe2a7edc28d7c5e92d24f95f5119de2f380030
MatthiasProbst <matth.probst@gmail.com>
2024-12-15 13:40:06 +0100

    add user test and missing test data

---
+++ tests/test_user.py
@@ -0,0 +1,46 @@
+import datetime
+import pathlib
+import shutil
+import unittest
+
+from piv2hdf import pivview, tutorial
+from piv2hdf.interface import PIVPlane, PIVMultiPlane
+
+__this_dir__ = pathlib.Path(__file__).parent
+
+CREATOR = {
+    'name': 'Matthias Probst',
+    'id': 'https://orcid.org/0000-0001-8729-0482',
+    'role': 'Researcher'}
+
+
+class TestUser(unittest.TestCase):
+
+    def test_tmpdir_change(self):
+        from piv2hdf import user
+        default_user_tmpdir = user.TEMPORARY_USER_DIRECTORY
+        plane_dirs = tutorial.PIVview.get_multiplane_directories()[0:2]
+
+        # init from folder:
+        plane_objs = [PIVPlane.from_folder(d, (datetime.datetime.now(), 5), pivview.PIVViewNcFile) for d in
+                      plane_dirs]
+        mplane = PIVMultiPlane(plane_objs)
+        hdf_filename = mplane.to_hdf(piv_attributes=dict(piv_medium='air', creator=CREATOR))
+        self.assertTrue(default_user_tmpdir in hdf_filename.parents)
+
+        new_tmp = __this_dir__ / 'tmp'
+        user.TEMPORARY_USER_DIRECTORY = new_tmp
+        plane_dirs = tutorial.PIVview.get_multiplane_directories()[0:2]
+
+        # init from folder:
+        plane_objs = [PIVPlane.from_folder(d, (datetime.datetime.now(), 5), pivview.PIVViewNcFile) for d in
+                      plane_dirs]
+        mplane = PIVMultiPlane(plane_objs)
+        hdf_filename = mplane.to_hdf(piv_attributes=dict(piv_medium='air',
+                                                         creator=CREATOR))
+        self.assertFalse(default_user_tmpdir in hdf_filename.parents)
+        self.assertTrue(new_tmp in hdf_filename.parents)
+
+    def tearDown(self):
+        new_tmp = __this_dir__ / 'tmp'
+        shutil.rmtree(new_tmp, ignore_errors=True)
