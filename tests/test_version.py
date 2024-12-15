59670c37be3b6bb18ff5bb8010a87b9501ae24ec
MatthiasProbst <matth.probst@gmail.com>
2024-12-15 14:55:59 +0100

    add CITATION.cff

--- tests/test_version.py
+++ tests/test_version.py
@@ -20,9 +20,20 @@ class TestVersion(unittest.TestCase):
         self.assertEqual(piv2hdf.__version__, this_version)
 
     def test_codemeta(self):
-        """checking if the version in codemeta.json is the same as the one of the toolbox"""
+        """checking if the version in codemeta.json is the same as the one of the piv2hdf"""
 
         with open(__this_dir__ / '../codemeta.json', 'r') as f:
             codemeta = json.loads(f.read())
 
         assert codemeta['version'] == piv2hdf.__version__
+
+    def test_citation_cff(self):
+        """checking if the version in CITATION.cff is the same as the one of the piv2hdf"""
+        this_version = 'x.x.x'
+        setupcfg_filename = __this_dir__ / '../CITATION.cff'
+        with open(setupcfg_filename, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if 'version: ' in line:
+                    this_version = line.split(':')[-1].strip()
+        self.assertEqual(piv2hdf.__version__, this_version)
